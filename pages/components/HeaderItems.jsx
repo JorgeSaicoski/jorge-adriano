e6eeb3b261959ce6db20b2cb0da17b9c05bc9b80
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-04-10 19:11:48 -0300

    Icon with className

--- pages/components/HeaderItems.jsx
+++ pages/components/HeaderItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeaderItems({Icon,title}) {
   return (
     <div className="flex flex-col justify-center items-center cursor-pointer hover:text-focus group">
-      <Icon/>
+      <Icon className="h-8 sm:h-10 mb-1 group-hover:animate-bounce"/>
       <p className="tracking-widest opacity-0 group-hover:opacity-100 text-xs sm:text-base">{title}</p>
     </div>
   )
