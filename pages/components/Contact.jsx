ffd09ddb5b4b9aa3278067dfedcf969db030d888
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-07-15 10:32:39 -0300

    Update Contact.jsx

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -73,7 +73,7 @@ function Contact() {
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-textColor sm:text-4xl">
             <span className="block">Phone: +598 93 309 600</span>
-            <span className="block text-ligth">E-mail: sarkis@jorgeadriano</span>
+            <span className="block text-ligth">E-mail: sarkis@jorgeadriano.com</span>
           </h2>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md shadow">
